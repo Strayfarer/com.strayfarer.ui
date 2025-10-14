eacb34e44f74976a3b0f4de8e8167cc040f67a28
Daniel Schulz <faulolio@gmail.com>
2025-10-14 15:55:19 +0200

    feat: FlexVectorImage

---
+++ Runtime/FlexVectorImage.cs
@@ -0,0 +1,57 @@
+using Unity.Properties;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Strayfarer.UI {
+    [UxmlElement]
+    public partial class FlexVectorImage : Image {
+        [Header(nameof(FlexVectorImage))]
+        [UxmlAttribute]
+        [CreateProperty]
+        public new ScaleMode scaleMode {
+            get => base.scaleMode;
+            set => base.scaleMode = value;
+        }
+
+        [UxmlAttribute]
+        [CreateProperty]
+        public new VectorImage vectorImage {
+            get => base.vectorImage;
+            set => base.vectorImage = value;
+        }
+
+        [UxmlAttribute]
+        [CreateProperty]
+        public new Color tintColor {
+            get => base.tintColor;
+            set => base.tintColor = value;
+        }
+
+        public FlexVectorImage() {
+            AddToClassList($"flexVectorImage");
+        }
+
+        protected override Vector2 DoMeasure(float desiredWidth, MeasureMode widthMode, float desiredHeight, MeasureMode heightMode) {
+            if (!vectorImage) {
+                return new(float.NaN, float.NaN);
+            }
+
+            if (vectorImage is not { height: > 0, width: > 0 }) {
+                return new(float.NaN, float.NaN);
+            }
+
+            float scaledWidth = desiredHeight * vectorImage.width / vectorImage.height;
+            float scaledHeight = desiredWidth * vectorImage.height / vectorImage.width;
+
+            return (widthMode, heightMode) switch {
+                (MeasureMode.AtMost, MeasureMode.AtMost) => scaledWidth <= desiredWidth
+                    ? new(scaledWidth, desiredHeight)
+                    : new(desiredWidth, scaledHeight),
+                (MeasureMode.Exactly, MeasureMode.Exactly) => new(desiredWidth, desiredHeight),
+                (MeasureMode.Exactly, _) => new(desiredWidth, scaledHeight),
+                (_, MeasureMode.Exactly) => new(scaledWidth, desiredHeight),
+                _ => new(float.NaN, float.NaN)
+            };
+        }
+    }
+}
